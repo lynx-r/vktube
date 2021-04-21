1b0440e7564f8ebd80f65e4c8994d5ec0eaf9243
lynx-r <lynx.p9@gmail.com>
2021-04-21 10:58:57 +0300

    storybook

---
+++ components/Button.stories.js
@@ -0,0 +1,25 @@
+import ButtonComponent from './Button'
+export default {
+  title: 'Button',
+  component: ButtonComponent,
+  argTypes: {
+    type: {
+      control: {
+        type: 'select',
+        options: ['primary', 'secondary'],
+      },
+      defaultValue: 'primary',
+    },
+    round: { control: 'boolean' },
+    text: {
+      control: 'text',
+      defaultValue: 'Awesome Button',
+    },
+  },
+}
+
+export const Button = (_, { argTypes }) => ({
+  components: { Button: ButtonComponent },
+  props: Object.keys(argTypes),
+  template: '<Button v-bind="$props" />',
+})
